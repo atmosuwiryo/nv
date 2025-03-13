f1fccf572a378dbc25b7205bd10322c36a14467a
abzcoding <abzcoding@gmail.com>
2025-03-13 18:48:55 +0330

    remove duplicate keymaps
    Signed-off-by: abzcoding <abzcoding@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -123,20 +123,6 @@ map("n", "<CR>", function()
   end
 end, { noremap = true, silent = true, nowait = true })
 
-local diagnostic_goto = function(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-  severity = severity and vim.diagnostic.severity[severity] or nil
-  return function()
-    go({ severity = severity, float = false })
-  end
-end
-map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
-map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
-map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 require("which-key").add({
   { "<leader>a", group = "Avante", icon = " " },
 })
