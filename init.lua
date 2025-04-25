473a9145cda33755a52e5860b29036902ef07a48
Atmo Suwiryo <suwiryo.atmo@gmail.com>
2025-04-25 14:43:07 -0400

    bump

--- init.lua
+++ init.lua
@@ -2,10 +2,25 @@
 require("config.lazy")
 
 
--- enable background transparancy for winteriscoming theme
+-- enable background transparency for all themes
 vim.cmd [[
   highlight Normal guibg=none
   highlight NonText guibg=none
   highlight Normal ctermbg=none
   highlight NonText ctermbg=none
+  highlight SignColumn guibg=none
+  highlight NormalFloat guibg=none
+  highlight FloatBorder guibg=none
+  highlight Pmenu guibg=none
+  highlight PmenuSel guibg=#3a3a3a
+  highlight CursorLine guibg=#1a1a1a
+  highlight LineNr guibg=none
+  highlight VertSplit guibg=none
+  highlight StatusLine guibg=none
+  highlight StatusLineNC guibg=none
+  highlight TabLine guibg=none
+  highlight TabLineFill guibg=none
+  highlight Comment gui=italic
+  highlight FoldColumn guibg=none
+  highlight Folded guibg=none
 ]]
