1f16ab304eea5ba9014bdeedd5890700338a8da2
abzcoding <abzcoding@gmail.com>
2025-04-26 13:57:23 +0330

    qflist actually works now
    Signed-off-by: abzcoding <abzcoding@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -115,23 +115,27 @@ map("n", "<A-Right>", ":vertical resize -2<CR>", opts)
 map("n", "<CR>", function()
   local cur_win = vim.api.nvim_get_current_win()
   local buf = vim.api.nvim_win_get_buf(cur_win)
-  if vim.bo[buf].buftype == "quickfix" then
+
+  if vim.bo[buf].buftype ~= "quickfix" then
+    vim.api.nvim_set_var("non_float_total", 0)
+    vim.cmd("silent! windo if &buftype != 'nofile' | let g:non_float_total += 1 | endif") -- Added silent! to avoid potential errors showing
+    vim.api.nvim_set_current_win(cur_win or 0)
+    if vim.api.nvim_get_var("non_float_total") == 1 then
+      if vim.fn.tabpagenr("$") == 1 then
+        return
+      end
+      vim.cmd("tabclose")
+    else
+      local last_cursor = vim.api.nvim_win_get_cursor(0)
+      pcall(vim.cmd, "tabedit %:p")
+      vim.api.nvim_win_set_cursor(0, last_cursor)
+    end
     return
   end
-  vim.api.nvim_set_var("non_float_total", 0)
-  vim.cmd("windo if &buftype != 'nofile' | let g:non_float_total += 1 | endif")
-  vim.api.nvim_set_current_win(cur_win or 0)
-  if vim.api.nvim_get_var("non_float_total") == 1 then
-    if vim.fn.tabpagenr("$") == 1 then
-      return
-    end
-    vim.cmd("tabclose")
-  else
-    local last_cursor = vim.api.nvim_win_get_cursor(0)
-    vim.cmd("tabedit %:p")
-    vim.api.nvim_win_set_cursor(0, last_cursor)
-  end
-end, { noremap = true, silent = true, nowait = true })
+
+  vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, false, true), 'nt')
+
+end, opts)
 
 require("which-key").add({
   { "<leader>a", group = "Avante", icon = " " },
